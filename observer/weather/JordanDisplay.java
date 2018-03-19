f890bc337fc95351e508d67a3a2f20047dd8d0c1
Jordan Luciano <lucianoj@moravian.edu>
2018-03-19 19:26:22 -0400

    Jordan added her changes

---
+++ observer/weather/JordanDisplay.java
@@ -0,0 +1,31 @@
+package observer.weather;
+
+import java.util.*;
+
+public class JordanDisplay implements Observer, DisplayElement {
+  private float humidity;
+	private WeatherData weatherData;
+
+	public JordanDisplay(WeatherData weatherData) {
+		this.weatherData = weatherData;
+		weatherData.registerObserver(this);
+	}
+
+	public void update(float temp, float humidity, float pressure) {
+    this.humidity = humidity;
+		display();
+	}
+
+	public void display() {
+    if(humidity < 70){
+      System.out.println("You won't sweat too much today");
+    }
+    else if(humidity >= 70 < 90){
+      System.out.println("It's really steamy out there");
+    }
+    else {
+		System.out.println("Drink lots of water! You'll be drenched!");
+    }
+  }
+
+}
--- observer/weather/WeatherStation.java
+++ observer/weather/WeatherStation.java
@@ -1,3 +1,4 @@
+
 package observer.weather;
 
 import java.util.*;
@@ -6,11 +7,12 @@ public class WeatherStation {
 
 	public static void main(String[] args) {
 		WeatherData weatherData = new WeatherData();
-	
-		CurrentConditionsDisplay currentDisplay = 
+
+		CurrentConditionsDisplay currentDisplay =
 			new CurrentConditionsDisplay(weatherData);
 		StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
 		ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
+		JordanDisplay jordanDisplay = new JordanDisplay(weatherData);
 
 		weatherData.setMeasurements(80, 65, 30.4f);
 		weatherData.setMeasurements(82, 70, 29.2f);
