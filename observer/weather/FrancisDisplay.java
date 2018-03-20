66bc4dfe38eea9eeb137bebdba7cc21f2093388d
Frank Jannuzzi <stfmj01@moravian.edu>
2018-03-20 10:29:49 -0400

    Francis made changes

---
+++ observer/weather/FrancisDisplay.java
@@ -0,0 +1,31 @@
+package observer.weather;
+
+import java.util.*;
+
+public class FrancisDisplay implements Observer, DisplayElement {
+    private float humidity;
+    private WeatherData weatherData;
+
+    public FrancisDisplay(WeatherData weatherData) {
+	this.weatherData = weatherData;
+	weatherData.registerObserver(this);
+    }
+
+    public void update(float temp, float humidity, float pressure) {
+	this.humidity = humidity;
+	display();
+    }
+
+    public void display() {
+	if(temp < 35){
+            System.out.println("There may be a chance of snow!");
+        }
+        else if(temp >= 35 < 50){
+            System.out.println("It's getting warmer out.");
+        }
+        else {
+            System.out.println("No need for longsleeves!");
+        }
+    }
+
+}
